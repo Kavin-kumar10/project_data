55bbfec7f6f89115df0a97ee4c239970fbefd9ca
kavinkumar10 <kavinkumar.prof@gmail.com>
2024-10-04 10:05:23 +0530

    express server

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const jsonServer = require("json-server"); 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000; 
+const express = require('express');
+const app = express();
+const port = 8000;
+const data = require("./db.json");
+const cors = require('cors');
 
-server.use(middlewares);
-server.use(router);
+app.use(cors());
+app.use(express.json());
 
-server.listen(port);
+app.get('/thumbnails', (req, res) => {
+  res.json(data.thumpnails);
+});
+
+app.get('/designs', (req, res) => {
+  res.json(data.designs);
+});
+
+app.get('/profiles', (req, res) => {
+  res.json(data.profiles);
+});
+
+app.get('/login', (req, res) => {
+    const { email, password } = req.query;
+    console.log('Email:', email, 'Password:', password); // Debugging
+    
+    const user = data.users.find(
+      user => user.email === email && user.password === password
+    );
+  
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
